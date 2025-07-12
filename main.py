d05c36b4a949d8f70895eea1c3863b1289a77222
Sergei Maslennikov <sergeimaslennikov89@gmail.com>
2025-07-12 19:28:11 +0300

    Change some things

--- main.py
+++ main.py
@@ -7,12 +7,17 @@ if __name__ == '__main__':
         calculator.Car("Toyota Corolla", 120000, 7,
                        1200, 2500)
     )
+
     calc.add_car(
         calculator.ElectricCar("Tesla Model 3", 200000,
                                5500, 150)
     )
+
     calc.add_car(
         calculator.Car("Range Rover", 650000, 3,
+                       3000, 7000)
+    )calc.add_car(
+        calculator.Car("Audi", 500000, 3,
                        3000, 7000)
     )
 
